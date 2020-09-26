bb255329bd1783a3363c015cdd5520658ceb861c
Raunaq Bahl <r.bahl@uqconnect.edu.au>
2020-09-26 12:02:58 +1000

    Updates

---
+++ precache-manifest.a9b8dc0e77b04a485f9fc811b3b23e3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94aa3aec4af2fa342d03ffd2ebf303bb",
+    "url": "/robo-friends-react/index.html"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robo-friends-react/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ba36890e900c050d4eea",
+    "url": "/robo-friends-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robo-friends-react/static/js/2.f690e5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robo-friends-react/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba36890e900c050d4eea",
+    "url": "/robo-friends-react/static/js/main.94989ac5.chunk.js"
+  },
+  {
+    "revision": "cc20bc9d5952885ce2b7",
+    "url": "/robo-friends-react/static/js/runtime-main.fefb5611.js"
+  }
+]);
