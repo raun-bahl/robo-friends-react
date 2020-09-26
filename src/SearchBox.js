2d735c101166b4ce4989c9687cd13fdd5feab99b
Raunaq Bahl <r.bahl@uqconnect.edu.au>
2020-09-26 11:40:28 +1000

    robo friends react app

---
+++ src/SearchBox.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SearchBox = ({searchfield, searchChange}) => {
+
+	return (
+		<div className = "pa2">
+		<input className = "pa2" type = 'search' placeholder = 'search robots' onChange = {searchChange} />
+		</div>
+	);
+}
+
+export default SearchBox;
